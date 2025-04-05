3684479457db8000ae466192f332db84f26f0299
P. Wydra <pwydra@gmail.com>
2025-04-05 17:13:50 -0400

    [#20] refactors daos to handle created_by;returning db result

--- internal/dao/muscleDao_test.go
+++ internal/dao/muscleDao_test.go
@@ -5,8 +5,10 @@ import (
 	"database/sql"
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"github.com/pwydra/shred/internal/model"
 	"github.com/stretchr/testify/assert"
@@ -22,8 +24,8 @@ func TestGetMuscleByCode(t *testing.T) {
 	musCode := "LATISSIMUS"
 	mock.ExpectQuery("SELECT \\* FROM muscle_type WHERE muscle_code = \\$1").
 		WithArgs(musCode).
-		WillReturnRows(sqlmock.NewRows([]string{"muscle_code", "muscle_name", "muscle_description", "muscle_group"}).
-			AddRow(musCode, "Latissimus", "Muscle of the back", "Back"))
+		WillReturnRows(sqlmock.NewRows([]string{"muscle_code", "muscle_name", "muscle_description", "muscle_group", "created_by", "created_at", "updated_at"}).
+			AddRow(musCode, "Latissimus", "Muscle of the back", "Back", uuid.New(), time.Now(), time.Now()))
 
 	muscle, err := dao.GetMuscleByCode(musCode)
 	assert.NoError(t, err)
@@ -70,13 +72,14 @@ func TestGetAllMuscles(t *testing.T) {
 }
 
 func TestCreateMuscle(t *testing.T) {
+	timeNow := time.Now()
 	db, mock, err := sqlmock.New()
 	assert.NoError(t, err)
 	defer db.Close()
 
 	dao := NewMuscleDAO(sqlx.NewDb(db, "postgres"))
 
-	catReq := &model.MuscleRequest{
+	musReq := &model.MuscleRequest{
 		MuscleFields: model.MuscleFields{
 			MuscleCode: "LAT",
 			MuscleName: "Latissimus",
@@ -84,12 +87,17 @@ func TestCreateMuscle(t *testing.T) {
 		},
 	}
 
-	mock.ExpectExec("INSERT INTO muscle_type \\( muscle_code, muscle_name, muscle_description, muscle_group \\) VALUES \\( \\$1, \\$2, \\$3, \\$4 \\)").
-		WithArgs(catReq.MuscleCode, catReq.MuscleName, catReq.MuscleDesc).
-		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectQuery("INSERT INTO muscle_type \\( muscle_code, muscle_name, muscle_description, muscle_group, created_by \\) VALUES \\( \\$1, \\$2, \\$3, \\$4, \\$5 \\).*").
+		WithArgs(musReq.MuscleCode, musReq.MuscleName, musReq.MuscleDesc, musReq.MuscleGroup, musReq.CreatedBy).
+		WillReturnRows(sqlmock.NewRows([]string{"created_at", "updated_at"}).AddRow(timeNow, timeNow))
 
-	err = dao.CreateMuscle(catReq)
+	mus, err := dao.CreateMuscle(musReq)
 	assert.NoError(t, err)
+	assert.Equal(t, mus.MuscleCode, musReq.MuscleCode)
+	assert.Equal(t, mus.MuscleName, musReq.MuscleName)
+	assert.Equal(t, mus.MuscleDesc, musReq.MuscleDesc)
+	assert.Equal(t, mus.CreatedAt, timeNow)
+	assert.Equal(t, mus.UpdatedAt, timeNow)
 }
 
 func TestCreateMuscle_Error(t *testing.T) {
@@ -99,7 +107,7 @@ func TestCreateMuscle_Error(t *testing.T) {
 
 	dao := NewMuscleDAO(sqlx.NewDb(db, "postgres"))
 
-	catReq := &model.MuscleRequest{
+	musReq := &model.MuscleRequest{
 		MuscleFields: model.MuscleFields{
 			MuscleCode: "LAT",
 			MuscleName: "Latissimus",
@@ -107,11 +115,11 @@ func TestCreateMuscle_Error(t *testing.T) {
 		},
 	}
 
-	mock.ExpectExec("INSERT INTO muscle_type \\( muscle_code, muscle_name, muscle_description, muscle_group \\) VALUES \\( \\$1, \\$2, \\$3, \\$4 \\)").
-		WithArgs(catReq.MuscleCode, catReq.MuscleName, catReq.MuscleDesc).
+	mock.ExpectQuery("INSERT INTO muscle_type \\( muscle_code, muscle_name, muscle_description, muscle_group, created_by \\) VALUES \\( \\$1, \\$2, \\$3, \\$4, \\$5 \\).*").
+		WithArgs(musReq.MuscleCode, musReq.MuscleName, musReq.MuscleDesc, musReq.MuscleGroup, musReq.CreatedBy).
 		WillReturnError(errors.New("insertion error"))
 
-	err = dao.CreateMuscle(catReq)
+	_, err = dao.CreateMuscle(musReq)
 	assert.Error(t, err)
 	assert.Equal(t, "insertion error", err.Error())
 }
@@ -123,7 +131,7 @@ func TestUpdateMuscle(t *testing.T) {
 
 	dao := NewMuscleDAO(sqlx.NewDb(db, "postgres"))
 
-	catReq := &model.MuscleRequest{
+	musReq := &model.MuscleRequest{
 		MuscleFields: model.MuscleFields{
 			MuscleCode:  "LAT",
 			MuscleName:  "Latissimus",
@@ -133,10 +141,10 @@ func TestUpdateMuscle(t *testing.T) {
 	}
 
 	mock.ExpectExec("UPDATE muscle_type SET muscle_name = \\$1, muscle_description = \\$2, muscle_group = \\$3 WHERE muscle_code = \\$4").
-		WithArgs(catReq.MuscleName, catReq.MuscleDesc, catReq.MuscleGroup, catReq.MuscleCode).
+		WithArgs(musReq.MuscleName, musReq.MuscleDesc, musReq.MuscleGroup, musReq.MuscleCode).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
-	err = dao.UpdateMuscle(catReq)
+	err = dao.UpdateMuscle(musReq)
 	assert.NoError(t, err)
 }
 
@@ -147,7 +155,7 @@ func TestUpdateMuscle_Error(t *testing.T) {
 
 	dao := NewMuscleDAO(sqlx.NewDb(db, "postgres"))
 
-	catReq := &model.MuscleRequest{
+	musReq := &model.MuscleRequest{
 		MuscleFields: model.MuscleFields{
 			MuscleCode:  "LAT",
 			MuscleName:  "Latissimus",
@@ -157,10 +165,10 @@ func TestUpdateMuscle_Error(t *testing.T) {
 	}
 
 	mock.ExpectExec("UPDATE").
-		WithArgs(catReq.MuscleName, catReq.MuscleDesc, catReq.MuscleGroup, catReq.MuscleCode).
+		WithArgs(musReq.MuscleName, musReq.MuscleDesc, musReq.MuscleGroup, musReq.MuscleCode).
 		WillReturnError(sqlmock.ErrCancelled)
 
-	err = dao.UpdateMuscle(catReq)
+	err = dao.UpdateMuscle(musReq)
 	assert.Error(t, err)
 	assert.Equal(t, "canceling query due to user request", err.Error())
 }
@@ -172,7 +180,7 @@ func TestUpdateMuscle_NotFound(t *testing.T) {
 
 	dao := NewMuscleDAO(sqlx.NewDb(db, "postgres"))
 
-	catReq := &model.MuscleRequest{
+	musReq := &model.MuscleRequest{
 		MuscleFields: model.MuscleFields{
 			MuscleCode:  "INVALID",
 			MuscleName:  "Invalid",
@@ -182,10 +190,10 @@ func TestUpdateMuscle_NotFound(t *testing.T) {
 	}
 
 	mock.ExpectExec("UPDATE muscle_type SET muscle_name = \\$1, muscle_description = \\$2, muscle_group = \\$3 WHERE muscle_code = \\$4").
-		WithArgs(catReq.MuscleName, catReq.MuscleDesc, catReq.MuscleGroup, catReq.MuscleCode).
+		WithArgs(musReq.MuscleName, musReq.MuscleDesc, musReq.MuscleGroup, musReq.MuscleCode).
 		WillReturnResult(sqlmock.NewResult(0, 0))
 
-	err = dao.UpdateMuscle(catReq)
+	err = dao.UpdateMuscle(musReq)
 	assert.Error(t, err)
 	assert.Equal(t, "muscle with Code INVALID not found", err.Error())
 }
